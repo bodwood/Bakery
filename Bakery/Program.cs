e74eec18986d82412b4b81bd1f2967e93dac6aca
bodwood <bodiewood@hotmail.com>
2022-12-09 15:03:43 -0800

    update Program.cs to allow users to select a pastery, bread load, or if they would like to exit the applicaiton. If the user enters something invalid, then they will be asked to enter a valid entry

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,7 +10,34 @@ namespace Bakery.Program
       Console.WriteLine("------------------------------------------------------");
       Console.WriteLine(Bread.greetingCustomer + "\n" + Bread.discountGreeting);
       Console.WriteLine("------------------------------------------------------");
-      Console.WriteLine("Would you like to purchase?\nEnter 'B' for bread loafs \nEnter 'P' for pasteries");
+      Console.WriteLine("What would you like to purchase?\nEnter 'B' for bread loafs \nEnter 'P' for pasteries\nEnter 'E' to exit");
+
+      char userChoice = char.Parse(Console.ReadLine().ToLower().Trim());
+      createOrder(userChoice);
     }
+    public static void createOrder(char userChoice)
+    {
+   
+      if (userChoice == 'b')
+      {
+        Console.WriteLine("\nHow many loafs of bread would you like to purchase?");
+        
+      }
+      else if (userChoice == 'p')
+      {
+        Console.WriteLine("\nHow many pasteries would you like to purchase?");
+      }
+      else if (userChoice == 'e')
+      {
+        System.Environment.Exit(0);
+      }
+      else{
+        Console.WriteLine("\n!!Please enter a valid input:!!\n'B' for bread loafs\n'P' for pasteries\n'E' to exit application");
+        userChoice = char.Parse(Console.ReadLine().ToLower().Trim());
+        createOrder(userChoice);
+      }
+
+    }
+
   }
 }
